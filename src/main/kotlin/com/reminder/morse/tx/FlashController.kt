759cfde1531ed3606aa2045e3d31ed1973754eba
bonnypanicker <bonnypanicker@outlook.com>
2026-02-27 08:29:13 +0530

    Fix_FlashController_crash_handling

--- src/main/kotlin/com/reminder/morse/tx/FlashController.kt
+++ src/main/kotlin/com/reminder/morse/tx/FlashController.kt
@@ -1,13 +1,18 @@
 package com.reminder.morse.tx
 
 import android.hardware.camera2.CameraManager
+import android.util.Log
 
 class FlashController(
     private val cameraManager: CameraManager,
     private val cameraId: String
 ) {
     fun setFlash(on: Boolean) {
-        cameraManager.setTorchMode(cameraId, on)
+        try {
+            cameraManager.setTorchMode(cameraId, on)
+        } catch (e: Exception) {
+            Log.e("FlashController", "Failed to set flash: ${e.message}")
+        }
     }
 
     fun turnOff() {
